6a7ee6a3307b777579bfa5b780668d800b31bbaa
MSI\stkkr <stkkr.1224@gmail.com>
2020-10-06 14:27:20 +0900

    リファクタリングその１ ・クラス名変更 ・クラスの定義を別のcpp、hに移動 ・よく使うヘッダーファイルのインクルードを、Common.hに集約

--- 01_1006/ClassPlactice/ClassPlactice/ClassPlactice.cpp
+++ 01_1006/ClassPlactice/ClassPlactice/ClassPlactice.cpp
@@ -1,43 +1,50 @@
 ﻿
-#include <stdio.h>
-#include <stdlib.h>
+#include "Common.h"
+#include "Unit.h"
 
-class Fighter
-{
-public:
-    Fighter();
-    
-    ~Fighter();
-    int m_hp;
-    int m_str;
-    int m_mag;
-    int m_tec;
-    int m_spd;
-    int m_def;
-    int m_luc;
-    int m_mde;
-};
+/*
+    リファクタリングとしてやってほしいこと
+      ・クラス名をunitという、よりふさわしいものに変更する
+      ・unit.cppとunit.hを作成して、クラスの宣言と実装をそちらに移す
+      ・stdlib.hとstdio.hはcommon.hにまとめる
+*/
 
-Fighter::Fighter()
-{
-    m_hp = 0;
-    m_str = 0;
-    m_mag = 0;
-    m_tec = 0;
-    m_spd = 0;
-    m_def = 0;
-    m_luc = 0;
-    m_mde = 0;
-}
-
-Fighter::~Fighter()
-{
-
-}
+//class Fighter
+//{
+//public:
+//    Fighter();
+//    
+//    ~Fighter();
+//    int m_hp;
+//    int m_str;
+//    int m_mag;
+//    int m_tec;
+//    int m_spd;
+//    int m_def;
+//    int m_luc;
+//    int m_mde;
+//};
+//
+//Fighter::Fighter()
+//{
+//    m_hp = 0;
+//    m_str = 0;
+//    m_mag = 0;
+//    m_tec = 0;
+//    m_spd = 0;
+//    m_def = 0;
+//    m_luc = 0;
+//    m_mde = 0;
+//}
+//
+//Fighter::~Fighter()
+//{
+//
+//}
 
 int main()
 {
-    Fighter fighter;
+    Unit fighter;
 
     printf("m_hp = %d\n", fighter.m_hp);
     printf("m_str = %d\n", fighter.m_str);
---
+++ 01_1006/ClassPlactice/ClassPlactice/Unit.h
@@ -0,0 +1,22 @@
+#ifndef Unit_H_
+#define Unit_H_
+
+class Unit
+{
+public:
+    Unit();
+
+    ~Unit();
+    int m_hp;
+    int m_str;
+    int m_mag;
+    int m_tec;
+    int m_spd;
+    int m_def;
+    int m_luc;
+    int m_mde;
+};
+
+
+#endif
+
